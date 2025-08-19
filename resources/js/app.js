35f67cf9c3c548cfa23b96f77b51ae5aae6052e6
boris2442 <aubinborissimotsebo@gmail.com>
2025-08-19 09:41:04 +0200

    remove particule.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import './bootstrap';
 // Importer Particles.js
-import particles from 'particles.js';
+// import particles from 'particles.js';
 import Alpine from 'alpinejs';
-window.onload = function() {
-    particlesJS.load('particles-js', 'particles.json', function() {
-        console.log('Particles.js config loaded');
-    });
-};
+// window.onload = function() {
+//     particlesJS.load('particles-js', 'particles.json', function() {
+//         console.log('Particles.js config loaded');
+//     });
+// };
 window.Alpine = Alpine;
 
 Alpine.start();
